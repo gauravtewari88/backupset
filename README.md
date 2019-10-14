79ec5b3c1bbf327c05f46640227aaa9339dccead
GAURAV TEWARI <gaurav.tewari@baesystems.com>
2019-10-14 10:06:49 +0800

    Backup Details in SQL Server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# backupset-
+# backupset
+
+Get Details of Backup in SQL Server. A backup set contains the backup from a single, successful backup operation. RESTORE, RESTORE FILELISTONLY, RESTORE HEADERONLY,and RESTORE VERIFYONLY statements operate on a single backup set within the media set on the specified backup device or devices.
---
+++ backupset.sql
@@ -0,0 +1,24 @@
+SELECT TOP ( 30 ) 
+ s.database_name, 
+ m.physical_device_name, 
+ cast(CAST(s.backup_size / 1000000 AS INT) as varchar(14)) 
+ + ' ' + 'MB' as bkSize, 
+ CAST(DATEDIFF(ss, s.backup_start_date, 
+ s.backup_finish_date) AS VARCHAR(4)) + ' ' 
++ 'Seconds' TimeTaken, 
+ s.backup_start_date, 
+ CAST(s.first_lsn AS varchar(50)) AS first_lsn, 
+ CAST(s.last_lsn AS varchar(50)) AS last_lsn, 
+ CASE s.[type] 
+ WHEN 'D' THEN 'Full' 
+WHEN 'I' THEN 'Differential' 
+WHEN 'L' THEN 'Transaction Log' 
+END as BackupType, 
+ s.server_name, 
+ s.recovery_model 
+ FROM msdb.dbo.backupset s
+ inner join msdb.dbo.backupmediafamily m ON s.media_set_id = m.media_set_id 
+WHERE s.database_name = 'Database Name' 
+ ORDER BY backup_start_date desc, backup_finish_date
+
+
